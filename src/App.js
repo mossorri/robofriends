eef5d55187e0bd5b9b083818c5dc4c082abccdee
mossorri <emmanuelkusiboamah@gmail.com>
2023-08-26 14:08:45 -0700

    updated folder structure && styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
+import CardList from "./Components/CardList";
 // import { robots } from "./robots";
-import SearchBox from "./Searchbox";
+import SearchBox from "./Components/Searchbox";
 import "./App.css";
 
 class App extends Component {
@@ -16,7 +16,7 @@ class App extends Component {
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then(users => this.setState({ robots: users }))
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -28,13 +28,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc mt7">Loding...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
---
+++ src/Scroll.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+
+// function Scroll(props) {
+//   return (
+//     <div
+//       style={{
+//         overflow: "scroll",
+//         border: "5px solid #000",
+//         height: "950px",
+//         marginTop:"10px"
+//       }}
+//     >
+//       {props.children}
+//     </div>
+//   );
+// }
+
+// export default Scroll;
