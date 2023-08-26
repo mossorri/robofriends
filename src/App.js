e0109b0fbb7ce5d9685ea2d90850812b68dc0891
mossorri <emmanuelkusiboamah@gmail.com>
2023-08-26 12:02:16 -0700

    added fetch json from api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
+// import { robots } from "./robots";
 import SearchBox from "./Searchbox";
+import "./App.css";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox/>
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then(users => this.setState({ robots: users }))
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/Searchbox.jsx
+++ src/Searchbox.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const SearchBox = ()=>{
-    return(
-        <div >
-            <input className='pa3 ba b--green bg-lightest-blue' type="search" placeholder='search robots'/>
+function SearchBox({searchfield,searchChange}) {
+    return (
+        <div>
+            <input
+                className="pa3 ba b--green bg-lightest-blue"
+                type="search"
+                placeholder="search robots"
+                onChange={searchChange} />
         </div>
-    )
+    );
 }
 
 export default SearchBox;
